faba301b400c8414afe56e16ce90a76a52b58505
Priyanshi Jain <priyanshijain1265@gmail.com>
2025-12-14 01:06:04 +0530

    added tech fact widget

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 createImageWidget("assets/images/leaderb.jpg", 5, 10, 320, 1);
 createImageWidget("assets/images/windchime.png", 40, 15, 360, 2);
 
-createRandomFactWidget(55, 40, 3);
+createTechFactWidget(15, 37, 3);
 createJSONWidget(45, 19, 2, "Elixir", "Lightweight and Unique", "Exploring basics");
-createJSONWidget(7, 5, 1, "React", "component-based UI", "Actively using");
+createJSONWidget(10, 5, 1, "React", "component-based UI", "Actively using");
 createQuoteWidget("Everything will be fine", 75, 5, 4);
