e7a13ab4f1e4a6db088fb08ba1755139ca22792c
Priyanshi Jain <priyanshijain1265@gmail.com>
2025-12-13 23:40:23 +0530

    improved the currently exploring widget

--- js/main.js
+++ js/main.js
@@ -2,5 +2,6 @@ createImageWidget("assets/images/leaderb.jpg", 5, 10, 320, 1);
 createImageWidget("assets/images/windchime.png", 40, 15, 360, 2);
 
 createRandomFactWidget(55, 40, 3);
-createJSONWidget(65, 20, 2);
+createJSONWidget(45, 19, 2, "Elixir", "Lightweight and Unique", "Exploring basics");
+createJSONWidget(7, 5, 1, "React", "component-based UI", "Actively using");
 createQuoteWidget("Everything will be fine", 75, 5, 4);
--- widgets/jsonWidget.js
+++ widgets/jsonWidget.js
@@ -1,20 +1,33 @@
-function createJSONWidget(x, y, z) {
-  const data = {
-    topic: "WebGL Shaders",
-    mood: "curious",
-    updated: "just now"
-  };
-
+function createJSONWidget(x, y, z, a, b, c) {
   const el = document.createElement("div");
   el.className = "widget json-widget";
 
   el.innerHTML = `
-{
-  "exploring": "${data.topic}",
-  "mood": "${data.mood}",
-  "updated": "${data.updated}"
-}
-`;
+    <div class="json-block">
+      <div class="json-line">
+        <span class="brace">{</span>
+      </div>
+
+      <div class="json-line indent">
+        <span class="key">"framework"</span><span class="colon">:</span>
+        <span class="value">"${a}"</span><span class="comma">,</span>
+      </div>
+
+      <div class="json-line indent">
+        <span class="key">"inspiration"</span><span class="colon">:</span>
+        <span class="value">"${c}"</span><span class="comma">,</span>
+      </div>
+
+      <div class="json-line indent">
+        <span class="key">"current_status"</span><span class="colon">:</span>
+        <span class="value">"${b}"</span>
+      </div>
+
+      <div class="json-line">
+        <span class="brace">}</span>
+      </div>
+    </div>
+  `;
 
   place(el, x, y, z);
   document.getElementById("canvas").appendChild(el);
