31c3b44ad6732924c989f4050a53db66c1aa4e6f
Priyanshi Jain <priyanshijain1265@gmail.com>
2025-12-21 23:29:51 +0530

    added the galaxy background

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-createImageWidget("assets/images/leaderb.jpg", 5, 10, 320, 1);
-createImageWidget("assets/images/windchime.png", 40, 15, 360, 2);
+// createImageWidget("assets/images/leaderb.jpg", 5, 10, 320, 1);
+// createImageWidget("assets/images/windchime.png", 40, 15, 360, 2);
 
-createTechFactWidget(15, 37, 3);
-createJSONWidget(45, 19, 2, "Elixir", "Lightweight and Unique", "Exploring basics");
-createJSONWidget(10, 5, 1, "React", "component-based UI", "Actively using");
+createTechFactWidget(15, 70, 0);
+createJSONWidget(55, 29, 2, "Elixir", "Lightweight and Unique", "Exploring basics");
+createJSONWidget(13, 10, 1, "React", "component-based UI", "Actively using");
 createQuoteWidget("Everything will be fine", 75, 5, 4);
-createSpotifyWidget(7,20);
+createSpotifyWidget(7,45);
